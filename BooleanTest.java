f0d9b72a3340e9083e06acfa9275bec60d910c3e
code-with-nanda <yogananda2206@gmail.com>
2022-08-04 19:53:27 +0530

    first commit

---
+++ BooleanTest.java
@@ -0,0 +1,11 @@
+class BooleanTest
+{
+ public static void main(String args[])
+ {
+   boolean a=true;
+   boolean b=True;
+   boolean c=TRUE;
+   
+   System.out.println("a="+a  "b="+b  "c="+c);
+ }
+}
